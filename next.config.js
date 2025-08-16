1c67d521bccc938cfbc510326f746eb6ca09f192
uPrayers <uprayersmail@gmail.com>
2025-08-16 16:43:00 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // IMPORTANT: do NOT set `output: 'export'`
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'uprayers.com' }],
+        destination: 'https://www.uprayers.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
