0a853042ea09fbf366fe92fa6751b6ab576189e0
uPrayers <uprayersmail@gmail.com>
2025-08-17 14:46:50 -0400

    Update SiteFooter.tsx

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,7 +1,10 @@
 export default function SiteFooter() {
   return (
-    <footer className="text-center text-sm py-6 text-gray-500 bg-white border-t">
-      © {new Date().getFullYear()} uPrayers.com — A place for every faith
+    <footer className="text-center text-sm text-gray-500 bg-white border-t">
+      <div className="py-8 flex flex-col items-center space-y-4">
+        {/* You could drop another subtle link or tagline here if desired */}
+        <div>© {new Date().getFullYear()} uPrayers.com — A place for every faith</div>
+      </div>
     </footer>
   );
 }
